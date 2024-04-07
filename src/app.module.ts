f3a2cebb3a1f8528c3c3a035ab7d6c7b1e92b628
wardSultan <wardalsultan96@gmail.com>
2024-04-07 04:16:10 +0300

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://wardalsultan96:H1wz8RMpAIx2IjFE@cluster0.r2sp8ph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get<string>('DB_USERNAME')}:${configService.get<string>('DB_PASSWORD')}@cluster0.r2sp8ph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+      }),
+      inject: [ConfigService],
+    }),
     UserModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
+  // dotenv.config({ path: '../.env' });
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const PORT = config.get('PORT') ?? 3000;
+
+  await app.listen(PORT);
 }
 bootstrap();
